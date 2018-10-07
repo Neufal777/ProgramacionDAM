2fc724b90e1651dae51b25778b5b25cc7c82bcf0
Naoufal Dahouli <neufal777@gmail.com>
2018-10-07 12:45:06 +0200

    Files

---
+++ Index.java
@@ -0,0 +1,18 @@
+public class Index{
+
+  public static void main(String[] args){
+
+        //variables que contindran les dades
+        String nomDia = "dijous";
+        String nomMes = "juliol";
+        int dia = 21;
+        int any = 2016;
+
+        System.out.println("Sistema Americà : " + nomDia + ","+ nomMes + " "+ dia + "," + any);
+        System.out.println("Sistema Europeu : " + nomDia + "," + dia + " " + nomMes + " " + any);
+
+
+
+  }//public static void
+
+}//Index
